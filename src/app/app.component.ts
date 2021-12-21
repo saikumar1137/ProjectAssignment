60f1a1fe5d8a049b7204f20d9e0367783dca7ab1
Sai_Kumar Pulugu <saikumarpspk1919@gmail.com>
2021-12-21 20:22:15 +0530

    New app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  form:FormGroup;
+  enable:boolean=false;
+  users: any;
+  
+  state:any;
+  namevalue:any;
   title = 'projectAssignment';
+  flag: boolean =false;
+  
+  constructor(private fb: FormBuilder,private http: HttpClient){
+    this.form = this.fb.group({
+      Name:['',Validators.required],
+      City:['',Validators.required],
+     
+    })
+  }
+ 
+  getresult(){
+    this.http.get("https://api.openbrewerydb.org/breweries?by_city="+this.form.value.City).subscribe(res=>{
+      this.users = res;
+      this.state=this.users[2]['state'];
+    })
+
+    this.http.get("https://api.openbrewerydb.org/breweries?by_name="+this.form.value.Name).subscribe(res=>{
+      this.users = res;
+      this.namevalue=this.users.length;
+    })
+    this.flag=true;
+
+    setTimeout(()=>{
+    this.form.reset();
+    this.flag=false;
+
+    }, 8000)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,HttpClientModule
+    
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
